78cc0cebe76060cc3a82b337aea64189ec3b61a9
Guilherme Carini <carinigcontact@gmail.com>
2021-04-10 12:06:40 -0300

    .

--- Infra/kafka/producer.go
+++ Infra/kafka/producer.go
@@ -7,9 +7,14 @@ import (
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+// NewKafkaProducer creates a ready to go kafka.Producer instance
 func NewKafkaProducer() *ckafka.Producer {
 	configMap := &ckafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"security.protocol": os.Getenv("security.protocol"),
+		"sasl.mechanisms":   os.Getenv("sasl.mechanisms"),
+		"sasl.username":     os.Getenv("sasl.username"),
+		"sasl.password":     os.Getenv("sasl.password"),
 	}
 	p, err := ckafka.NewProducer(configMap)
 	if err != nil {
@@ -17,6 +22,8 @@ func NewKafkaProducer() *ckafka.Producer {
 	}
 	return p
 }
+
+// Publish is simple function created to publish new message to kafka
 func Publish(msg string, topic string, producer *ckafka.Producer) error {
 	message := &ckafka.Message{
 		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
@@ -25,7 +32,6 @@ func Publish(msg string, topic string, producer *ckafka.Producer) error {
 	err := producer.Produce(message, nil)
 	if err != nil {
 		return err
-
 	}
 	return nil
 }
